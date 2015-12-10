045a212738b94bd433fad323483e591f35e00661
Xenakios <xenakios@gmail.com>
2015-12-11 01:59:57 +0200

    Changes to allow building on OSX. The SWELL GUI however doesn't actually run when the action is called in Reaper.

--- header/MyFirstDialog.h
+++ header/MyFirstDialog.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#ifdef WIN32
-#include "windows.h"
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include "WDL/WDL/swell/swell.h"
 #endif
 
 HWND open_my_first_modeless_dialog(HWND parent);
