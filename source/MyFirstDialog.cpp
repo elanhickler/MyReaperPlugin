91a74e298d1b33fa070111d9d0518be7b14866fb
xenakios <xenakios@gmail.com>
2015-12-17 03:23:23 +0200

    Remove the old dialog procedure code

--- source/MyFirstDialog.cpp
+++ source/MyFirstDialog.cpp
@@ -159,77 +159,7 @@ void set_selected_take_name_from_line_edit(HWND hwndDlg)
 	}
 }
 
-INT_PTR CALLBACK myfirstdialogproc(
-	HWND   hwndDlg,
-	UINT   uMsg,
-	WPARAM wParam,
-	LPARAM lParam
-	)
-{
-	// The "canonical" way is to use a switch case for the messages, but ifs will work fine too
-	// and it's less likely to do some stupid mistakes this way
-	if (uMsg == WM_INITDIALOG) // The very first message sent for the window
-	{
-		ShowWindow(hwndDlg, SW_SHOW);
-		// SetWindowText is a "polymorphic" API that can change the visible text of various things,
-		// including window titles or text edit box texts
-		SetWindowText(hwndDlg, "My First Dialog");
-		//SetWindowPos(hwndDlg,0,20,80,200,100,SWP_NOZORDER|SWP_NOACTIVATE);
-		return TRUE;
-	}
-	// WM_CLOSE sent at least when the window close button is pressed
-	if (uMsg == WM_CLOSE)
-	{
-		DestroyWindow(hwndDlg);
-		return TRUE;
-	}
-	// Button clicks and various other things will cause a WM_COMMAND message to be sent
-	if (uMsg == WM_COMMAND)
-	{
-		// Check the source of the message is our button and that the message is the button click
-		if (LOWORD(wParam) == IDC_DO1 && HIWORD(wParam) == BN_CLICKED)
-		{
-			if (CountSelectedMediaItems(nullptr) > 0)
-			{
-				MediaItem* item = GetSelectedMediaItem(nullptr, 0);
-				MediaItem_Take* take = GetActiveTake(item);
-				if (take != nullptr)
-				{
-					char buf[1024];
-					if (GetSetMediaItemTakeInfo_String(take, "P_NAME", buf, false)==true)
-						SetWindowText(GetDlgItem(hwndDlg, IDC_EDIT1), buf);
-				}
-				
-			}
-			else 
-				SetWindowText(GetDlgItem(hwndDlg, IDC_EDIT1), "No item selected!");
-			
-		}
-		if (LOWORD(wParam) == IDC_DO2 && HIWORD(wParam) == BN_CLICKED)
-		{
-			if (CountSelectedMediaItems(nullptr) > 0)
-			{
-				MediaItem* item = GetSelectedMediaItem(nullptr, 0);
-				MediaItem_Take* take = GetActiveTake(item);
-				if (take != nullptr)
-				{
-					char buf[1024];
-					GetWindowText(GetDlgItem(hwndDlg, IDC_EDIT1), buf, 1024);
-					GetSetMediaItemTakeInfo_String(take, "P_NAME", buf, true);
-					UpdateArrange();
-				}
-			}
-		}
-		return TRUE; // return true when we did handle the message
-	}
-	return FALSE; // return false when we didn't handle the message
-}
-#ifdef OLD_MY_FIRST_DIALOG
-HWND open_my_first_modeless_dialog(HWND parent)
-{
-	return CreateDialogParam(g_hInst, MAKEINTRESOURCE(IDD_DIALOG1), parent, myfirstdialogproc, NULL);
-}
-#else
+
 std::unique_ptr<ReaperDialog> g_my_dialog;
 Timer g_test_timer;
 HWND open_my_first_modeless_dialog(HWND parent)
@@ -244,7 +174,6 @@ HWND open_my_first_modeless_dialog(HWND parent)
 		}
 		if (msg == WM_CLOSE)
 		{
-			g_test_timer.stop();
 			ShowWindow(hwnd, SW_HIDE);
 			return true;
 		}
@@ -278,7 +207,6 @@ HWND open_my_first_modeless_dialog(HWND parent)
 		g_my_dialog->setVisible(true);
 	return g_my_dialog->getWindowHandle();
 }
-#endif
 
 std::vector<std::unique_ptr<TestControl>> g_testcontrols;
 
