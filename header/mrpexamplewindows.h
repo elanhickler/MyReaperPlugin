85efba123a4b924d679a9f74c61d666c7e5b9a81
xenakios <xenakios@gmail.com>
2015-12-25 03:53:34 +0200

    Clean up mrpwindows h and cpp from all the messy testing code and move those to mrpexamplewindows h and cpp

--- header/mrpexamplewindows.h
+++ header/mrpexamplewindows.h
@@ -32,4 +32,33 @@ private:
 	std::vector<slider_controls> m_sliders;
 };
 
+class TestMRPModalWindow : public MRPWindow
+{
+public:
+	void init_modal_dialog();
+	std::shared_ptr<WinLineEdit> m_line_edit;
+
+};
+
+class TestMRPPWindow : public MRPWindow
+{
+public:
+	TestMRPPWindow(HWND parent, std::string title = "Untitled");
+	~TestMRPPWindow()
+	{
+		readbg() << "TestMRPWindow dtor\n";
+	}
+	void resized() override;
+private:
+	std::shared_ptr<WinComboBox> m_combo1;
+	std::shared_ptr<WinComboBox> m_combo2;
+	std::shared_ptr<ReaSlider> m_slider1;
+	std::shared_ptr<EnvelopeControl> m_envcontrol1;
+	std::shared_ptr<WinLabel> m_label1;
+	std::shared_ptr<WinLineEdit> m_edit1;
+};
+
+void show_modal_dialog(HWND parent);
+
 HWND toggle_sliderbank_window(HWND parent);
+HWND open_win_controls_window(HWND parent);
--- header/mrpwindows.h
+++ header/mrpwindows.h
@@ -65,33 +65,4 @@ protected:
 	ModalResult m_modal_result = Rejected;
 };
 
-class TestMRPModalWindow : public MRPWindow
-{
-public:
-	void init_modal_dialog();
-	std::shared_ptr<WinLineEdit> m_line_edit;
-	
-};
-
-class TestMRPPWindow : public MRPWindow
-{
-public:
-	TestMRPPWindow(HWND parent, std::string title = "Untitled");
-	~TestMRPPWindow()
-	{
-		readbg() << "TestMRPWindow dtor\n";
-	}
-	void resized() override;
-private:
-	std::shared_ptr<WinComboBox> m_combo1;
-	std::shared_ptr<WinComboBox> m_combo2;
-	std::shared_ptr<ReaSlider> m_slider1;
-	std::shared_ptr<EnvelopeControl> m_envcontrol1;
-	std::shared_ptr<WinLabel> m_label1;
-	std::shared_ptr<WinLineEdit> m_edit1;
-};
-
-HWND open_win_controls_window(HWND parent);
-void show_modal_dialog(HWND parent);
-
 void shutdown_windows();
