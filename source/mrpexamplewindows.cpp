6fc5c4b2b62e2bcae1d7744a0d750f310c624872
xenakios <xenakios@gmail.com>
2015-12-25 03:21:26 +0200

    Remove MyFirstDialog files from VS project

--- source/mrpexamplewindows.cpp
+++ source/mrpexamplewindows.cpp
@@ -113,3 +113,47 @@ HWND toggle_sliderbank_window(HWND parent)
 	g_sliderbankwindow->setVisible(true);
 	return g_sliderbankwindow->getWindowHandle();
 }
+
+#ifdef FOOFOOZ
+// keep this around for slightly longer
+INT_PTR CALLBACK xycontroldlgproc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == WM_INITDIALOG)
+	{
+		SetWindowText(hwndDlg, "Multi XY control");
+		g_xycontrol = std::make_unique<TestControl>(hwndDlg);
+		g_xycontrol->PointMovedCallback = [ptr = g_xycontrol.get()](int ptindex, double x, double y)
+		{
+			fx_param_t* x_target = ptr->getFXParamTarget(ptindex, 0);
+			fx_param_t* y_target = ptr->getFXParamTarget(ptindex, 1);
+			if (x_target != nullptr && y_target != nullptr)
+			{
+				if (x_target->tracknum >= 1)
+					TrackFX_SetParamNormalized(GetTrack(nullptr, x_target->tracknum - 1),
+						x_target->fxnum, x_target->paramnum, x);
+				if (y_target->tracknum >= 1)
+					TrackFX_SetParamNormalized(GetTrack(nullptr, y_target->tracknum - 1),
+						y_target->fxnum, y_target->paramnum, y);
+			}
+		};
+		ShowWindow(hwndDlg, SW_SHOW);
+		return TRUE;
+	}
+	if (uMsg == WM_CLOSE)
+	{
+		ShowWindow(hwndDlg, SW_HIDE);
+		return TRUE;
+	}
+	if (uMsg == WM_SIZE)
+	{
+		RECT r;
+		GetClientRect(hwndDlg, &r);
+		int w = r.right - r.left;
+		int h = r.bottom - r.top;
+		g_xycontrol->setBounds(0, 0, w, h);
+		InvalidateRect(hwndDlg, NULL, TRUE);
+		return TRUE;
+	}
+	return FALSE;
+}
+#endif
