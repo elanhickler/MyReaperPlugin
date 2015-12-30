7822a0342047dfddee398b2a7f7c25827e9a003e
xenakios <xenakios@gmail.com>
2015-12-30 12:38:16 +0200

    Looks like Unicode characters pass through the ReaScripting stuff correctly. Noticed the Reaper scripting IDE does not support Unicode, though.

--- reascript/lua/unicode_test.lua
+++ reascript/lua/unicode_test.lua
@@ -1,3 +1,23 @@
-﻿reaper.ShowConsoleMsg("きょ とフ")
+﻿thewindow=nil
+function tick()
+  if reaper.MRP_WindowIsClosed(thewindow) then
+    reaper.MRP_DestroyWindow(thewindow)
+    return
+  end
+  if reaper.MRP_WindowIsDirtyControl(thewindow,"Αντίθετα με") then
+    reaper.ShowConsoleMsg("Button named Αντίθετα με was pressed\n")
+  end
+  reaper.MRP_WindowClearDirtyControls(thewindow)
+  reaper.defer(tick)
+end
+
+thewindow=reaper.MRP_CreateWindow("Test Window : びょ以苩珥滯 じ")
+reaper.MRP_WindowAddControl(thewindow,"Button","Ääkkösnappula")
+reaper.MRP_SetControlBounds(thewindow,"Ääkkösnappula",5,5,150,20)
+reaper.MRP_WindowAddControl(thewindow,"Button","Αντίθετα με")
+reaper.MRP_SetControlBounds(thewindow,"Αντίθετα με",5,30,150,20)
+
+tick()
+
 
 
